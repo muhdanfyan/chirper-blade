6ac7f3c3c0b03f0262c9fdc113a0e6a3f7232531
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2024-12-22 12:50:29 +0800

    Update index.blade.php

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -4,7 +4,7 @@
             @csrf
             <textarea
                 name="message"
-                placeholder="{{ __('What\'s on your mind?') }}"
+                placeholder="{{ __('Ahlan wa sahlan?') }}"
                 class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
             >{{ old('message') }}</textarea>
             <x-input-error :messages="$errors->get('message')" class="mt-2" />
